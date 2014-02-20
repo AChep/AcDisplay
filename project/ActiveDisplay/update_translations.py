88c7a4154f67bb62a02d47b5610ac2bf15346772
Artem Chepurnoy <artemchep@gmail.com>
2014-02-20 22:23:47 +0200

    add basic translations check, write some output to console

--- project/ActiveDisplay/update_translations.py
+++ project/ActiveDisplay/update_translations.py
@@ -1,9 +1,14 @@
 import os
 import shutil
 import urllib
-from zipfile import ZipFile
 import errno
 import time
+import re
+import sys
+from zipfile import ZipFile
+
+TRANSLATION_FILE_NAME = 'translations-snapshot'
+MAX_DOWNLOADING_TRIES = 5
 
 def make_sure_path_exists(path):
     try:
@@ -12,20 +17,48 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
-# Download latest translations' build
-urllib.urlretrieve('https://crowdin.net/download/project/acdisplay.zip', 'translations-snapshot.zip')
+# Try to download latest translations' build from
+# Crowdin.
+print 'Downloading latest build from Crowdin...'
+for i in range(1, MAX_DOWNLOADING_TRIES):
+    try:
+        urllib.urlretrieve('https://crowdin.net/download/project/acdisplay.zip', TRANSLATION_FILE_NAME + '.zip')
+        break
+    except IOError as exception:
+        pass
+else:
+    print 'E: Failed to download translations! Terminating...'
+    sys.exit()
 
 # Where to extract it?
-translDir = os.path.abspath('translations-snapshot')
+translDir = os.path.abspath(TRANSLATION_FILE_NAME)
 if os.path.exists(translDir):
         shutil.rmtree(translDir)
 
 # Unzip translations archive
-zipTest = ZipFile('translations-snapshot.zip')
+print 'Unpacking archive...'
+zipTest = ZipFile(TRANSLATION_FILE_NAME + '.zip')
 zipTest.extractall(translDir)
 zipTest.close()
 
+# Check if some translations are broken.
+print 'Searching for broken translations:'
+for path, dirs, files in os.walk(translDir):
+    for filename in files:
+        fullpath = os.path.join(path, filename)
+        fullpathstr = str(os.path.join(path, filename))
+
+        # Log every file.
+        print 'Processing file: ', fullpathstr[
+            fullpathstr.find(
+                os.path.basename(translDir)):]
+        for line in open(fullpath):
+            if re.search('(\\$|%)\\s', line):
+                print 'Problematic line: ' + line
+                input('Press <Enter> to continue...')
+
 # Copy translations to project
+print 'Copying translations to project...'
 localizedProjectDir = os.path.abspath('src/localized/res')
 if os.path.exists(localizedProjectDir):
         shutil.rmtree(localizedProjectDir)
@@ -34,9 +67,15 @@ make_sure_path_exists(localizedProjectDir)
 for filename in os.listdir(translDir):
         if filename == 'ru' or filename == 'en':
                 # Those translations are default and provided by me.
-                continue        
+                continue
+
+        # Check for broken formatting
         os.rename(os.path.join(translDir, filename), os.path.join(localizedProjectDir, 'values-' + filename.replace('-', '-r')))
 
 # Remove temp files
-os.remove('translations-snapshot.zip')
-shutil.rmtree(translDir)
+print 'Removing temp files...'
+os.remove(TRANSLATION_FILE_NAME + '.zip')
+shutil.rmtree(TRANSLATION_FILE_NAME)
+
+print ''
+print 'Build and enjoy. :)'
