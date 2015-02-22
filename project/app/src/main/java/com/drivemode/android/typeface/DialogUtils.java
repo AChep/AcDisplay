d9fc48c09bd94eb4718872d19145db7e4cecf2bc
Artem Chepurnoy <artemchep@gmail.com>
2015-02-22 23:24:40 +0200

    Use a local copy of the TypefaceHelper

---
+++ project/app/src/main/java/com/drivemode/android/typeface/DialogUtils.java
@@ -0,0 +1,47 @@
+package com.drivemode.android.typeface;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * @author KeishinYokomaku
+ */
+final class DialogUtils {
+	public static <D extends Dialog> void setTypeface(TypefaceHelper helper, D dialog, String typefaceName, int style) {
+		if (dialog instanceof ProgressDialog) {
+			setTypeface(helper, (ProgressDialog) dialog, typefaceName, style);
+		} else if (dialog instanceof AlertDialog) {
+			setTypeface(helper, (AlertDialog) dialog, typefaceName, style);
+		}
+	}
+
+	private static void setTypeface(TypefaceHelper helper, AlertDialog alertDialog, String typefaceName, int style) {
+		Button positive = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+		Button negative = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+		Button neutral = alertDialog.getButton(DialogInterface.BUTTON_NEUTRAL);
+		TextView message = (TextView) alertDialog.findViewById(android.R.id.message);
+		if (positive != null) {
+			helper.setTypeface(positive, typefaceName, style);
+		}
+		if (negative != null) {
+			helper.setTypeface(negative, typefaceName, style);
+		}
+		if (neutral != null) {
+			helper.setTypeface(neutral, typefaceName, style);
+		}
+		if (message != null) {
+			helper.setTypeface(message, typefaceName, style);
+		}
+	}
+
+	private static void setTypeface(TypefaceHelper helper, ProgressDialog progressDialog, String typefaceName, int style) {
+		TextView message = (TextView) progressDialog.findViewById(android.R.id.message);
+		if (message != null) {
+			helper.setTypeface(message, typefaceName, style);
+		}
+	}
+}
---
+++ project/app/src/main/java/com/drivemode/android/typeface/TypefaceCache.java
@@ -0,0 +1,57 @@
+package com.drivemode.android.typeface;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.Hashtable;
+
+/**
+ * This is a typeface instance cache.
+ * The cache is to avoid memory leak problem when a typeface is loaded.
+ * See the link for more details about the memory leak issue.
+ * (https://code.google.com/p/android/issues/detail?id=9904)
+ *
+ * @author hnakagawa
+ */
+/* package */ final class TypefaceCache {
+	private static TypefaceCache sInstance;
+
+	private final Hashtable<String, Typeface> mCache = new Hashtable<String, Typeface>();
+
+	private final Application mApplication;
+
+	private TypefaceCache(Application application) {
+		mApplication = application;
+	}
+
+	/**
+	 * If the cache has an instance for the typeface name, this will return the instance immediately.
+	 * Otherwise this method will create typeface instance and put it into the cache and return the instance.
+	 * @param name the typeface name.
+	 * @return {@link android.graphics.Typeface} instance.
+	 */
+	public synchronized Typeface get(String name) {
+		Typeface typeface = mCache.get(name);
+		if(typeface == null) {
+			try {
+				typeface = Typeface.createFromAsset(mApplication.getAssets(), name);
+			} catch (Exception exp) {
+				return null;
+			}
+			mCache.put(name, typeface);
+		}
+		return typeface;
+	}
+
+	/**
+	 * Retrieve this cache.
+	 * @param context the context.
+	 * @return the cache instance.
+	 */
+	public static synchronized TypefaceCache getInstance(Context context) {
+		if (sInstance == null)
+			sInstance = new TypefaceCache((Application)context.getApplicationContext());
+		return sInstance;
+	}
+}
