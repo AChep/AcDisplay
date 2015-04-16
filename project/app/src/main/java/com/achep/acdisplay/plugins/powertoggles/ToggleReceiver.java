0cd0532879e3ee5c8e887d70152f5bc30614e4e0
Artem Chepurnoy <artemchep@gmail.com>
2015-04-16 22:13:13 +0300

    ToggleReceiver: Support async Config's work

--- project/app/src/main/java/com/achep/acdisplay/plugins/powertoggles/ToggleReceiver.java
+++ project/app/src/main/java/com/achep/acdisplay/plugins/powertoggles/ToggleReceiver.java
@@ -19,21 +19,58 @@
 package com.achep.acdisplay.plugins.powertoggles;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.achep.acdisplay.Config;
+import com.achep.base.content.ConfigBase;
 import com.painless.pc.PowerTogglesPlugin;
+
+import static com.achep.base.Build.DEBUG;
 
 /**
  * AcDisplay toggle plugin for PowerToggles <http://powertoggles.com>
  *
  * @author Artem Chepurnoy
  */
-public class ToggleReceiver extends PowerTogglesPlugin {
+public class ToggleReceiver extends PowerTogglesPlugin implements ConfigBase.OnConfigChangedListener {
+    private static final String TAG = "ToggleReceiver";
+
+    /**
+     * An object to monitor the garbage collector.
+     */
+    /* This is possible only because the Config wraps listeners
+     * in a weak references. If it changes -> we will get a memory leak. */
+    private final Object dFinilizer = new Object() {
+
+        @Override
+        protected void finalize() throws Throwable {
+            try {
+                Config.getInstance().unregisterListener(ToggleReceiver.this);
+                if (DEBUG) Log.d(TAG, "Un-registering the toggle receiver from Config.");
+            } finally {
+                super.finalize();
+            }
+        }
+    };
+
+    public ToggleReceiver() {
+        Config.getInstance().registerListener(this);
+    }
 
     @Override
     protected void changeState(Context context, boolean newState) {
         Config.getInstance().setEnabled(context, newState, null);
-        sendStateUpdate(context, Config.getInstance().isEnabled());
     }
 
+    @Override
+    public void onConfigChanged(@NonNull ConfigBase config,
+                                @NonNull String key,
+                                @NonNull Object value) {
+        switch (key) {
+            case Config.KEY_ENABLED:
+                sendStateUpdate(config.getContext(), (Boolean) value);
+                break;
+        }
+    }
 }
